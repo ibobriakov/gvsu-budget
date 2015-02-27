4e27b191f663f11ce72ffd21121e98f2185f307c
yarnaid <yarnaid@gmail.com>
2015-02-27 10:06:45 +0300

    format currency fix

--- js/statistics.js
+++ js/statistics.js
@@ -191,7 +191,7 @@ function formatcurrency(value) {
         return "N/A";
     } else if (value >= 1000000000) {
 	    return "$ " + Math.round(value / 1000000000).toString() + " B";
-    } else if (value >= 1000000) {
+    } else if (value >= 1000000 && value < 1000000000) {
         return "$" + Math.round(value / 1000000).toString() + " M";
     } else if (value < 1000000 && value >= 1000) {
         return "$" + Math.round(value / 1000).toString() + " K";
--- js/years_chart.js
+++ js/years_chart.js
@@ -1,9 +1,9 @@
 function format_shorten(value) {
     if (value === undefined) {
         return "N/A";
-    } else if (value >= 100000000) {
-        return Math.round(value / 100000000).toString() + " B";
-    } else if (value >= 1000000) {
+    } else if (value >= 1000000000) {
+        return Math.round(value / 1000000000).toString() + " B";
+    } else if (value >= 1000000 && value < 1000000000) {
         return Math.round(value / 1000000).toString() + " M";
     } else if (value < 1000000 && value >= 1000) {
         return Math.round(value / 1000).toString() + " K";
